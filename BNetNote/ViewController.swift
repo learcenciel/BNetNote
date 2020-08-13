d15a1b527c1d6a00a148a4450e4319040e67e0f3
Learcenciel <croleguu@mail.ru>
2020-08-13 14:58:00 +0700

    Initial setup (pods, project vc setup, etc.)

---
+++ BNetNote/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  BNetNote
+//
+//  Created by Arview on 13.08.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .blue
+    }
+}
+
